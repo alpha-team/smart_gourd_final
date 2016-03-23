295796b1b369943f0bfeb34ead1eb732ceac36cd
alpha-team <mahanshayegh@gmail.com>
2016-03-23 15:25:21 +0430

    Update sbssbot.lua

--- bot/sbssbot.lua
+++ bot/sbssbot.lua
@@ -314,74 +314,73 @@ See Patterns In Github
     help_text = [[
  لیست دستورات :
 __________________________
-اخراج [آیدی،کد،ریپلای] 
-شخص مورد نظر از گروه اخراج ميشود.
-—-------------------
-بن [آیدی،کد،ریپلای]
-شخص مورد نظر از گروه تحریم میشود
-—-------------------
-حذف بن[کد]
-شخص مورد نظر از تحریم خارج ميشود
-—-------------------
-لیست بن
-لیست افرادی که از گروه تحریم شده اند
-—-------------------
-خروج : ترک گروه
-—------------------------------—
-صاحب : نمایش آیدی مدیر گروه
-—-------------------
-لیست : لیست کمک مدیرها
-—-------------------
-ترفیع [ریپلای،یوزرنیم]
-اضافه کردن کمک مدیر
-—-------------------
-تنزل [ریپلای،یوزرنیم]
-حذف کردن کمک مدیر
-—-------------------
-قفل [اعضا|نام|ربات |تگ|عکس|خروج|فحش]
-—----------------------
-باز کردن [اعضا|نام|ربات |تگ|عکس|خروج|فحش]
-—------------------------------—
-تنظیم عکس : اضافه کردن وقفل عکس گروه
-—----------------------
-تنظیم نام [نام]
-عوض کردن نام گروه
-—------------------------------—
-توضیحات: درباره گروه
-—----------------------
-قوانین: قوانین گروه
-—----------------------
-تنظیم قانون<متن>
-—----------------------
-تنظیم توضیحات<متن> 
-—------------------------------—
-تنظیمات: تنظیمات گروه
-—------------------------------—
-لینک جدید : تعویض لینک و ارسال درگروه
-—----------------------
-لینک خصوصی :ارسال در چت خصوصی 
-—------------------------------—
-لینک : لینک گروه
-—------------------------------—
-حساسیت[تعداد]
-محدودیت تعداد اسپم
-—------------------------------—
-پاک کردن 
-پاکسازی مدیرها/قوانین/موضوع
-—------------------------------—
-ایدی [یوزرنیم]
-بازگرداندن کد آیدی
-—----------------------
-امار : آمار در پیام ساده
-—------------------------------—
-تبدیل <متن>
-تبدیل متن به تصویر
-—------------------------------—
-بگو <متن> : تکرار متن
-—------------------------------—
-تگ : صدا کردن افراد گروه
-—---------------------—
-نیاز نیست از '!' و '/' استفاده کنید*
+!kick [username|id]
+You can also do it by reply
+!ban [ username|id]
+You can also do it by reply
+!unban [id]
+You can also do it by reply
+!who
+Members list
+!modlist
+Moderators list
+!promote [username]
+Promote someone
+!demote [username]
+Demote someone
+!kickme
+Will kick user
+!about
+Group description
+!setphoto
+Set and locks group photo
+!setname [name]
+Set group name
+!rules
+Group rules
+!id
+return group id or user id
+!help
+!lock [member|name]
+Locks [member|name] 
+!unlock [member|name|photo]
+Unlocks [member|name|photo]
+!set rules <text>
+Set <text> as rules
+!set about <text>
+Set <text> as about
+!settings
+Returns group settings
+!newlink
+create/revoke your group link
+!link
+returns group link
+!owner
+returns group owner id
+!setowner [id]
+Will set id as owner
+!setflood [value]
+Set [value] as flood sensitivity
+!stats
+Simple message statistics
+!save [value] <text>
+Save <text> as [value]
+!get [value]
+Returns text of [value]
+!clean [modlist|rules|about]
+Will clear [modlist|rules|about] and set it to nil
+!res [username]
+returns user id
+"!res @username"
+!log
+will return group logs
+!banlist
+will return group ban list
+**U can use both "/" and "!" 
+*Only owner and mods can add bots in group
+*Only moderators and owner can use kick,ban,unban,newlink,link,setphoto,setname,lock,unlock,set rules,set about and settings commands
+*Only owner can use res,setowner,promote,demote and log commands
+
 _______________________________
 Writed by erfan0953
 ]]
